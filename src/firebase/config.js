09c7d48524f0d9250ed25606a68996299377ed5f
Lucia Rojo <113059638+lrojo1@users.noreply.github.com>
2024-05-19 18:47:22 -0700

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -7,12 +7,7 @@ import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBi0ise3CIbLfv6hnvr4yPcPlTgqo7XdhE",
-    authDomain: "kerncater.firebaseapp.com",
-    projectId: "kerncater",
-    storageBucket: "kerncater.appspot.com",
-    messagingSenderId: "874600098586",
-    appId: "1:874600098586:web:ba30e06758e01e52029aa3"
+
 };
 
 // Initialize Firebase
